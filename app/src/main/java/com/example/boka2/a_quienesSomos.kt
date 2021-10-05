423ac4d579557467add0268960885f513023afae
noemimargamallo <43636627+noemimargamallo@users.noreply.github.com>
2021-10-05 10:02:29 +0200

    soy la mejor

---
+++ app/src/main/java/com/example/boka2/a_quienesSomos.kt
@@ -0,0 +1,11 @@
+package com.example.boka2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class a_quienesSomos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.p_quienes_somos)
+    }
+}
