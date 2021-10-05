ef0c4ce4c845d7aa906763852c5d4361345b0175
ikercalb <ikercaballero271@gmail.com>
2021-10-05 14:04:53 +0200

    menu hecho

--- app/src/main/java/com/example/boka2/a_localizacion.kt
+++ app/src/main/java/com/example/boka2/a_localizacion.kt
@@ -15,50 +15,52 @@ import kotlinx.android.synthetic.main.p_localizacion.*
 
 
 class a_localizacion : AppCompatActivity() {
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val iditem = item.getItemId()
+
+        if (iditem==R.id.carta){
+            val intent=Intent(this, a_carta::class.java)
+            startActivity(intent)
+        }
+        if (iditem==R.id.Localizar){
+            val intent= Intent(this, a_localizacion::class.java)
+            startActivity(intent)
+        }
+        if (iditem==R.id.Reservar){
+            val intent= Intent(this, a_reservas::class.java)
+            startActivity(intent)
+        }
+        /*  if (iditem==R.id.Calendario){
+               val intent=Intent(this, a_calendario::class.java)
+               startActivity(intent)
+           }*/
+        if (iditem==R.id.Quienes){
+            val intent=Intent(this, a_quienesSomos::class.java)
+            startActivity(intent)
+        }
+        if (iditem==R.id.Perfil){
+            val intent= Intent(this, a_perfil::class.java)
+            startActivity(intent)
+        }
+        if (iditem==R.id.sesion){
+
+            Toast.makeText(this, "Sesion cerrada", Toast.LENGTH_SHORT).show()
+            val intent= Intent(this, a_login::class.java)
+            finish()
+            startActivity(intent)
+        }
+
+        return true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        fun onCreateOptionsMenu(menu: Menu?): Boolean {
-            super.onCreateOptionsMenu(menu)
-            val inflater = menuInflater
-            inflater.inflate(R.menu.menu, menu)
-            return true
-        }
-        fun onOptionsItemSelected(item: MenuItem): Boolean {
-            val iditem = item.getItemId()
 
-            /*if (iditem==R.id.carta){
-                val intent=Intent(this, carta::class.java)
-                startActivity(intent)
-            }*/
-            if (iditem==R.id.Localizar){
-                val intent= Intent(this, a_localizacion::class.java)
-                startActivity(intent)
-            }
-            if (iditem==R.id.Reservar){
-                val intent= Intent(this, a_reservas::class.java)
-                startActivity(intent)
-            }
-            /* if (iditem==R.id.Calendario){
-                 val intent=Intent(this, ::class.java)
-                 startActivity(intent)
-             }*/
-            /*if (iditem==R.id.Quienes){
-                val intent=Intent(this, ::class.java)
-                startActivity(intent)
-            }*/
-            if (iditem==R.id.Perfil){
-                val intent= Intent(this, a_perfil::class.java)
-                startActivity(intent)
-            }
-            if (iditem==R.id.sesion){
-
-                Toast.makeText(this, "Sesion cerrada", Toast.LENGTH_SHORT).show()
-                finish()
-
-            }
-
-            return true
-        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.p_localizacion)
 
--- app/src/main/java/com/example/boka2/a_registro.kt
+++ app/src/main/java/com/example/boka2/a_registro.kt
@@ -1,9 +1,14 @@
 package com.example.boka2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 
 class a_registro : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.p_registro)
