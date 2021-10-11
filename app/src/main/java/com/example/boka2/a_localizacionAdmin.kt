202c0821592cae5f8b4761778280047e6c826ed4
ikercalb <ikercaballero271@gmail.com>
2021-10-11 11:37:59 +0200

    calendario y logo finish

---
+++ app/src/main/java/com/example/boka2/a_localizacionAdmin.kt
@@ -0,0 +1,11 @@
+package com.example.boka2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class a_localizacionAdmin : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.p_localizacion_admin)
+    }
+}
