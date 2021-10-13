474da20db3e78c7f05b4da5ec63ada1f42ad72ce
ikercalb <ikercaballero271@gmail.com>
2021-10-13 09:27:31 +0200

    splash a medias

---
+++ app/src/main/java/com/example/boka2/splash.kt
@@ -0,0 +1,15 @@
+package com.example.boka2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+
+    }
+}
