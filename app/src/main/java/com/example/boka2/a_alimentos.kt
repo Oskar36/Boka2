e022ab66f5b52c08eacfdf13fd70dcd3c934f33e
ikercalb <ikercaballero271@gmail.com>
2021-10-18 08:26:59 +0200

    la dificil pantalla alimentos

---
+++ app/src/main/java/com/example/boka2/a_alimentos.kt
@@ -0,0 +1,11 @@
+package com.example.boka2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class a_alimentos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.p_alimentos)
+    }
+}
